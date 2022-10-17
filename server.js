6fd0922116578e2a18713ed4d0aafc4ddb37a96f
jineu <a11718trannhuy@gmail.com>
2022-10-17 13:57:05 +0700

    eight

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const cors = require("cors")
 
 const mongoConnection = require('./config/MongoDB');
 const ImportData = require('./DataImport');
@@ -13,6 +14,8 @@ mongoConnection();
 const app = express();
 
 // 
+
+app.use(cors())
 
 app.use("/api/import", ImportData)
 
